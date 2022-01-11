b090ce7557e8d83866522ca2914fdb176744ac93
nikithay0200 <nikithay0200@gmail.com>
2022-01-11 18:58:06 +0530

    Added procfile and updated ports

--- server.js
+++ server.js
@@ -29,4 +29,8 @@ app.use((err, req, res, next) => {
 
 //assign port
 const port = 3000;
-app.listen(port, () => console.log(`server on ${port}...`))
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, () => console.log(`server has started successfully`))
